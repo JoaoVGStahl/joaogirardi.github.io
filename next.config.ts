09fd398ee97e2e1ec75d70d925808264cc548b2e
João Girardi <jvitorstahl@hotmail.com>
2025-04-29 01:21:57 -0300

    Updated NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "",
+  assetPrefix: "./",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
